5a6de8a7f16a4ad849a062f25411221a71e1c1c9
Aria Fatah <ariafatah0711@gmail.com>
2025-01-27 00:18:38 +0700

    first commit

---
+++ 01 - dasar/01 - hello.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+// include berfungsi untuk memanggil library
+// standar untuk input dan output (I/O)
+// printf() – untuk menampilkan output ke layar.
+// scanf() – untuk membaca input dari pengguna.
+// getchar() – untuk membaca satu karakter dari input.
+// putchar() – untuk menampilkan satu karakter ke layar.
+// fopen() – untuk membuka file.
+// fclose() – untuk menutup file.
+// fread() dan fwrite() – untuk membaca dan menulis data dari/ke file.
+
+int main() {
+    printf("hello world");
+    return 0;
+}
+
+
+// int main => function yang akan dipanggil ketika di run
